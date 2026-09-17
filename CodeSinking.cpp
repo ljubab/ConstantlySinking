4341d7b998f44feb8056878dabe6a254b24bbd32
Ljubomir Banovic <ljuba.banovic@gmail.com>
2026-09-18 00:00:15 +0200

    remove sideEffect check

--- CodeSinking.cpp
+++ CodeSinking.cpp
@@ -86,9 +86,6 @@ bool CodeSinking::runOnFunction(Function &F) {
         if(isa<AllocaInst>(CurrInst) || isa<BranchInst>(CurrInst) || isa<ReturnInst>(CurrInst))
             continue;
 
-        if(CurrInst->mayHaveSideEffects())
-            continue;
-
         BasicBlock *Dest = getDestination(*CurrInst, IfBlock, ElseBlock);
 
         if(!Dest)
